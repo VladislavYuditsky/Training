6eab2da4f4c915293ed2d5dac2551522ba1d124e
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-09-23 01:06:22 +0300

    Add branching

---
+++ JavaFundamentals/src/com/yuditsky/branching/Task35.java
@@ -0,0 +1,31 @@
+// Вычислить число и месяц в невисокосном году по номеру дня
+
+package com.yuditsky.branching;
+
+import java.util.Scanner;
+
+public class Task35 {
+    public static void main(String[] args) {
+        String[] months = {"January", "February", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December"};
+        int[] daysMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int dayPerYear = 365;
+        int dayNumber;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter day number: ");
+            dayNumber = in.nextInt();
+        }while(dayNumber > dayPerYear | dayNumber <= 0);
+
+        for(int i = 0; i < months.length; i++){
+            if(dayNumber > daysMonth[i]){
+                dayNumber -= daysMonth[i];
+            } else {
+                System.out.println(dayNumber + " " + months[i]);
+                break;
+            }
+        }
+    }
+
+}
