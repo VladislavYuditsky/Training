d64ecd72ade350347b6ed52bbdb1742fe988f66e
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-09-23 17:30:47 +0300

    Add one dimensional arrays

---
+++ JavaFundamentals/src/com/yuditsky/one_dimensional_arrays/Task17.java
@@ -0,0 +1,52 @@
+//Дана последовательность целых чисел a1,...,an. Образовать новую последовательность, выбросив из
+//исходной те члены, которые равны min(a1,...,an).
+
+package com.yuditsky.one_dimensional_arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task17 {
+    public static void main(String[] args) {
+        int[] array, newArray;
+        int diff = 0;
+        int minNum;
+        Random random = new Random();
+
+        array = new int[random.nextInt(9) + 1];
+
+
+        array[0] = random.nextInt(10);
+        minNum = array[0];
+        for(int i = 1; i < array.length; i++){
+            array[i] = random.nextInt(10);
+            if(minNum > array[i]){
+                minNum = array[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        int i = 0;
+        do{
+            if(array[i] == minNum){
+                diff++;
+                leftShift(array, i);
+            } else {
+                i++;
+            }
+        }while(i < array.length);
+
+        newArray = new int[array.length - diff];
+        System.arraycopy(array, 0, newArray, 0, newArray.length);
+        System.out.println(Arrays.toString(newArray));
+    }
+
+    public static void leftShift(int[] array, int startPosition){
+        int i;
+        for(i = startPosition; i < array.length - 1; i++){
+            array[i] = array[i + 1];
+        }
+        array[i] += 1;
+    }
+}
---
+++ JavaFundamentals/src/com/yuditsky/one_dimensional_arrays/Task19.java
@@ -0,0 +1,44 @@
+//В массиве целых чисел с количеством элементов n найти наиболее часто встречающееся число.
+// Если таких чисел несколько, то определить наименьшее из них.
+
+package com.yuditsky.one_dimensional_arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task19 {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Random random = new Random();
+        int num, quantity = 1;
+        int curNum, curQuantity;
+
+        size = random.nextInt(14) + 1;
+        System.out.println("Size of array: " + size);
+        array = new int[size];
+        for(int i = 0; i < size; i++){
+            array[i] = random.nextInt(10);
+        }
+        System.out.println("Array: "+ Arrays.toString(array));
+
+        num = array[0];
+        for(int i = 0; i < array.length; i++){
+            curNum = array[i];
+            curQuantity = 1;
+            for(int j = i + 1; j < array.length; j++){
+                if(curNum == array[j]){
+                    curQuantity++;
+                }
+            }
+            if(curQuantity > quantity){
+                num = curNum;
+                quantity = curQuantity;
+            }
+            if(curQuantity == quantity && curNum < num){
+                num = curNum;
+            }
+        }
+        System.out.println(num);
+    }
+}
